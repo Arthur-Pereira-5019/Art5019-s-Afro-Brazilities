20189be7b192b1ad955ef0a3d475fd66b2eaf624
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-08-03 20:07:01 -0300

    First commit, no much stuff at all, just testing with Neoforge and trying to create a conch item

---
+++ src/main/java/com/art5019/afrobrazilities/items/BuzioConch.java
@@ -0,0 +1,25 @@
+package com.art5019.afrobrazilities.items;
+
+import com.art5019.afrobrazilities.Art5019sAfrobrazilities;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import static com.art5019.afrobrazilities.Art5019sAfrobrazilities.ITEMS;
+import static com.art5019.afrobrazilities.Art5019sAfrobrazilities.MODID;
+
+@Mod(MODID)
+public class BuzioConch {
+    public static final DeferredItem<Item> BUZIO_CONCH = ITEMS.registerItem(
+            "buzio_conch",
+            Item::new, // The factory that the properties will be passed into.
+            new Properties()
+    );
+}
